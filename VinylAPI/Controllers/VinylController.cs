be68e4b6ff06f9a4ed67342af2f17688f398585b
Max Hallberg <maxhal0710@gmail.com>
2024-02-11 15:13:21 +0100

    fixed controller

--- VinylAPI/Controllers/VinylController.cs
+++ VinylAPI/Controllers/VinylController.cs
@@ -22,6 +22,56 @@ namespace VinylAPI.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task
+        public async Task<IActionResult> Get(int id)
+        {
+            var record = await _context.Records.FirstOrDefaultAsync(m => m.RecordId == id);
+            if (id == 0)
+                return Ok(record);
+            if (record == null)
+                return NotFound();
+            return Ok(record);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Record record)
+        {
+            if(record.RecordId == 0) 
+                return BadRequest();
+            _context.Add(record);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Record recordData)
+        {
+            if (recordData == null || recordData.RecordId == 0)
+                return BadRequest();
+
+            var record = await _context.Records.FindAsync(recordData.RecordId);
+            if (record == null)
+                return NotFound();
+            record.RecordId = recordData.RecordId;
+            record.RecordPictureUrl = recordData.RecordPictureUrl;
+            record.AlbumName = recordData.AlbumName;
+            record.ArtistName = recordData.ArtistName;
+            record.ReleaseYear = recordData.ReleaseYear;
+            record.AlbumSongs = recordData.AlbumSongs;
+            record.MyRating = recordData.MyRating;
+            record.WhatCollection = recordData.WhatCollection;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var record = _context.Records.FindAsync(id);
+            if (record.Result == null)
+                return NotFound(id);
+            _context.Records.Remove(record.Result);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
